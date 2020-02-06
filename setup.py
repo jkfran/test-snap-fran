7d5566ad7bd2aa8e083cb6149e79f62dc57859ca
Francisco Jimenez Cabrera <francisco.jimenez.cabrera@canonical.com>
2020-02-06 14:13:31 +0000

    Version 2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="test-snap-fran",
-    version="0.0.1",
+    version="0.0.2",
     author="Francisco Jimenez Cabrera",
     author_email="francisco.jimenez.cabrera@canonical.com",
     py_modules=["test_snap"],
